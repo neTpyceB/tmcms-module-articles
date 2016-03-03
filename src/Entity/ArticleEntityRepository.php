76a48e2a83de092f969667066536ac53f5e301af
alpha <alpha@devp.lv>
2016-03-03 15:23:16 +0200

    update

--- src/Entity/ArticleEntityRepository.php
+++ src/Entity/ArticleEntityRepository.php
@@ -12,7 +12,7 @@ use TMCms\Orm\EntityRepository;
  */
 class ArticleEntityRepository extends EntityRepository
 {
-    protected $translation_fields = ['title', 'description', 'text'];
+    protected $translation_fields = ['title', 'description', 'text', 'meta_keywords', 'meta_description'];
     protected $table_structure = [
         'fields' => [
             'category_id' => [
@@ -21,10 +21,19 @@ class ArticleEntityRepository extends EntityRepository
             'title' => [
                 'type' => 'translation',
             ],
+            'image' => [
+                'type' => 'varchar'
+            ],
             'description' => [
                 'type' => 'translation',
             ],
             'text' => [
+                'type' => 'translation',
+            ],
+            'meta_keywords' => [
+                'type' => 'translation',
+            ],
+            'meta_description' => [
                 'type' => 'translation',
             ],
             'ts_created' => [
